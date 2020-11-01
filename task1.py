4c12cd5d2ffe78cc7f54c0667a019bd5d836f8c7
isapoff <iskender.saparov.is@gmail.com>
2020-11-02 00:25:40 +0600

    initial commit

---
+++ task1.py
@@ -0,0 +1,93 @@
+# 1
+# day = int(input('Введите день: '))
+# month = int(input('Введите месяц: '))
+# year = int (input ("Введите год: "))
+
+# if year % 400 == 0:
+#     print(" високосный")
+# elif year % 100 == 0:
+#     print(" не високосный")
+# elif year % 4 == 0:
+#     print("високосный")
+# else:
+#     print("не високосный")
+
+
+# 2
+# given_number = int(input("Enter your number: "))
+
+# if given_number % 3 == 0 and given_number % 5 ==0:
+#     print("HahaHoo")
+# elif given_number % 3 == 0:
+#     print("Haha")
+# elif given_number % 5 == 0:
+#     print("Hoo")
+# elif  given_number % 3 != 0 and  given_number % 5 != 0:
+#     print("Aaaa")
+
+
+# 3
+# num1 = int(input("enter first number: "))
+# num2 = int(input("enter second number: ")) 
+# sum = (int(num1)+(num2))
+# if 5 < sum:
+#     print('Сумма чисел больше 5')
+# elif 5 > sum:
+#     print('Сумма чисел меньше 5')
+
+
+
+# 5
+# x = int(input('Введите число: '))
+# positive = 1
+# negative = -1
+# if x > 0:
+#     print(positive)
+# elif x < 0 or x == 0:
+#     print(negative)
+
+
+
+# 6
+# x = input()
+# if x.count('l') ==1:
+#     print(x.find('l'))
+# elif x.count('l') >=2:
+#     print(x.find('l'), x.rfind('l'))
+
+
+
+
+
+# 7
+# favorites = {'Джек Лондон': 'Зов природы', 'Чингиз Айтматов': 'Первый учитель'}
+# a = input('Введите автора: ')
+# if a in favorites:
+#     print (favorites.get('Джек Лондон'))
+
+# elif a not in favorites:
+#     print('Введите произведение: ')
+#     x = input()
+#     favorites[a] = x
+
+# print(favorites)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
