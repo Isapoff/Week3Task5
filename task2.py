71675f317d6b4a4d1ae4389d9709fcc4f1a75c5c
isapoff <iskender.saparov.is@gmail.com>
2020-11-02 00:27:03 +0600

    first commit

---
+++ task2.py
@@ -0,0 +1,58 @@
+# 1
+# a = input()
+# b = 0
+# for x in a:
+#     if int(x) > b:
+#        b = int(x)
+# print(b)
+
+# 2
+# import random
+ 
+# number = random.randint(0, 100)
+# while True:
+#     num = input('Enter your number: ')
+    
+#     if num == "" or num == "exit":
+#         print('exit from program')
+#         break
+    
+#     if not num.isdigit():
+#         print("Enter true number: ")
+#         continue
+#     num = int(num)
+#     if num == number:
+#         print('Right')
+#         break
+#     elif num > number:
+#         print('the planned number is less') 
+#     elif num < number:
+#         print('the planned number is greater')
+
+# 3
+
+# number = int(input('Введите число: '))
+# factorial = 1
+# for x in range(1,number+1):
+#     factorial *= x
+#     print(f'factorial = {factorial}')
+# print("Факториал числа", number, "равен", factorial)
+
+
+
+# 4
+# for x in range(1,10):
+#   for y in range(1,10):
+#     print(x*y, end='\t')
+      
+#   print()
+#   print()
+
+
+
+
+
+
+
+
+      
